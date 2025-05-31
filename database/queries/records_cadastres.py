7bc141d762d312e4f4375fd181d22adaf8ab26bc
TRIGUN-D <aka_gto@mail.ru>
2025-05-31 16:54:06 +0300

    добавим запросы в бд

--- database/queries/records_cadastres.py
+++ database/queries/records_cadastres.py
@@ -1,13 +1,13 @@
 from database.models import CadastreNumbers, ObjectCoordinates, RecordsCadastres
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from schemas.schemas_cadastre import RecordCadastre
+from schemas.schemas_cadastre import RecordCadastreInRedis
 
 
-async def get_cadastres_by_id(session: AsyncSession, cadastres_id: int) -> RecordCadastre | None:
+async def get_cadastres_by_id(session: AsyncSession, cadastres_id: int) -> RecordCadastreInRedis | None:
     return await session.get(RecordsCadastres, cadastres_id)
 
 
-async def add_record_cadastre(session: AsyncSession, record_cadastre:RecordCadastre) -> None:
+async def add_record_cadastre(session: AsyncSession, record_cadastre:RecordCadastreInRedis) -> int:
     pass
 
