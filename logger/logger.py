baa5014f3334a8ee027499f1775d2589597a96c9
TRIGUN-D <aka_gto@mail.ru>
2025-06-02 21:26:37 +0300

    сделали красивый логгер

--- logger/logger.py
+++ logger/logger.py
@@ -3,32 +3,65 @@ import typing
 import sys
 
 
+COMPLETE = 25
+
+
+class StdoutFormatter(logging.Formatter):
+    """Logging Formatter to add colors and count warning / errors"""
+
+    grey = '\033[90m'
+    yellow = '\033[93m'
+    white = '\33[97m'
+    red = '\033[91m'
+    green = '\033[92m'
+    blue = '\033[34m'
+
+    FORMATS = {
+        logging.DEBUG: white,
+        logging.INFO: blue,
+        COMPLETE: green,
+        logging.WARNING: yellow,
+        logging.ERROR: red,
+        logging.CRITICAL: red
+    }
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Format record."""
+        return f'{self.FORMATS.get(record.levelno)}{super(StdoutFormatter, self).format(record)}'
+
+
 class BaseLogger(logging.Logger):
     """
     Base Logger.
     """
+    _date_format = '%Y-%m-%d %H:%M:%S'
+
+    _handlers: typing.Dict[logging.Handler, logging.Formatter] = {}
 
     def __init__(
             self,
             name: str,
-            level: typing.Optional[int] = None,
-            printer: typing.Optional[logging.Handler] = None,
             **kwargs: typing.Any
     ) -> None:
         super().__init__(name)
-        self.setLevel(level=level)
+        self.setLevel(level=0)
+        self._handlers[logging.StreamHandler(stream=sys.stdout)] = StdoutFormatter(
+            fmt=f"[%(asctime)s][%(levelname)s]{name} >>>  %(message)s", datefmt=self._date_format)
 
-        datefmt = '%Y-%m-%d %H:%M:%S'
-        info = f"{'|'.join([str(x) for x in kwargs.values()] + [name])}"
-        self._handlers: typing.Dict[logging.Handler, logging.Formatter] = {}
-        self._printer = printer
-        if self._printer:
-            self._printer.setLevel(level=level)
-            self._handlers[self._printer] = logging.Formatter(
-                fmt=f"[{info}] >>> [%(levelname)s] %(message)s", datefmt=datefmt)
+    def callHandlers(self, record: logging.LogRecord) -> None:
+        """Call handlers for record."""
+        for handler in self._handlers:
+            self._handle_record(handler, record)
 
     def _handle_record(self, handler: logging.Handler, record: logging.LogRecord) -> None:
         msg = record.msg
         record.msg = self._handlers[handler].format(record)
         handler.handle(record)
         record.msg = msg
+
+    def complete(self, msg: str, *args: typing.Any, **kwargs: typing.Any) -> None:
+        """
+        Log with severity 'COMPLETE'.
+        """
+        if self.isEnabledFor(COMPLETE):
+            self._log(COMPLETE, msg, args, **kwargs)
