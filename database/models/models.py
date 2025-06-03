c3abe7ea03b6f7c0b12520a6959b94abad66cbdb
TRIGUN-D <aka_gto@mail.ru>
2025-06-04 00:45:08 +0300

    унифицироваали таблицы

--- database/models/models.py
+++ database/models/models.py
@@ -2,12 +2,12 @@ from sqlalchemy.orm import DeclarativeBase, declared_attr, Mapped, mapped_column
 from sqlalchemy import Integer, String
 
 from database.models._base_model import _BaseModel
+from enums import Stage
 
 
 class CadastreNumbers(_BaseModel):
     """
-    Cadastre Number schema
-    - Кадастровый номер
+    Cadastre Number table
     """
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     cadastre_a: Mapped[str]
@@ -18,8 +18,9 @@ class CadastreNumbers(_BaseModel):
 
 class ObjectCoordinates(_BaseModel):
     """
-    - Координата х
-    - Координата у
+    Coordinates model table
+    - Coordinate х
+    - Coordinate у
     """
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     coordinate_x: Mapped[float]
@@ -28,14 +29,10 @@ class ObjectCoordinates(_BaseModel):
 
 class RecordsCadastres(_BaseModel):
     """
-    Основная таблица
-
-    - Кадастровый номер
-    - Координаты х\у
+    Main table cadastre
     """
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     cadastre_number: Mapped['CadastreNumbers'] = relationship(foreign_keys='CadastreNumbers.id')
     object_coordinates: Mapped['ObjectCoordinates'] = relationship(foreign_keys='ObjectCoordinates.id')
     calculated: Mapped[bool]
-
-
+    stages: Mapped[Stage]
